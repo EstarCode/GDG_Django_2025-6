0526b7f68da53c22c492d60623faa531a80ec4df
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2025-12-30 11:25:30 +0300

    Delete parity.py

--- parity.py
+++
@@ -1,9 +0,0 @@
-def checkParity():
-    number = int(input("Enter an integer: "))
-    if number % 2 == 0:
-        print(f"{number} is even.")
-    else:
-        print(f"{number} is odd.")
-
-
-checkParity()
