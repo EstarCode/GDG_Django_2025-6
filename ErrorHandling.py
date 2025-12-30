44c453c560432ffb56ed1e111f3b34cd641bb740
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2025-12-30 10:52:05 +0300

    Delete ErrorHandling.py

--- ErrorHandling.py
+++
@@ -1,7 +0,0 @@
-try:
-    with open("abraham.txt", "r") as file:
-        content = file.read()
-        print(f"Wellcome  {content} ")
-except FileNotFoundError:
-    with open("abraham.txt", "w") as file:
-        file.write("Guest")
