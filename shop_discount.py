bd27f480beb6137bf48c17a62328cb1d83dd5348
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2025-12-31 00:10:32 +0300

    Delete shop_discount.py

--- shop_discount.py
+++
@@ -1,9 +0,0 @@
-def discount():
-    numberOfItem = int(input("Enter the number of items wanted to buy: "))
-    if numberOfItem > 3:
-        print("Discount applied")
-    else:
-        print("No discount")
-
-
-discount()
