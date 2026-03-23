ae9f5bef2f16cd0bf0b951edd3d378e2cfcd2553
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2026-03-23 22:36:54 +0000

    update views, urls and templates files

--- library_management/Library/urls.py
+++ library_management/Library/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', views.book_list, name='book-list'),
-    path("books/<int:book_id>/loan/", views.CreateLoanView.as_view(), name='create-loan'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:book_id>/borrow/', views.CreateLoanView.as_view(), name='create_loan'),
 ]
--- library_management/Library/views.py
+++ library_management/Library/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.db.models import Count, Q
-from .models import ...
+
 
 
 def book_list(request):
@@ -14,30 +14,31 @@ def book_list(request):
         "books": books
     }
 
-    return render(request, "books.html", context)
+    return render(request, "Library/books.html", context)
 
 
 
 class CreateLoanView(View):
-    def post(self, request, book_id, *args, **kwargs):
 
+    def post(self, request, book_id, *args, **kwargs):
         member_id = request.POST.get("member_id")
 
         if not member_id:
             return JsonResponse({"error": "member_id is required"}, status=400)
 
-        # book comes from URL
+        try:
+            member_id = int(member_id)
+        except ValueError:
+            return JsonResponse({"error": "Invalid member ID"}, status=400)
+
         book = get_object_or_404(Book, id=book_id)
         member = get_object_or_404(Member, id=member_id)
 
-        # check availability
         if book.available_copies <= 0:
             return JsonResponse({"error": "Book not available"}, status=400)
 
-        # create loan
         Loan.objects.create(book=book, member=member)
 
-        # update copies
         book.available_copies -= 1
         book.save()
 
