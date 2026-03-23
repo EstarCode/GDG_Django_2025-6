63afa6ed63ae230a0875ae098dcd3ac4793fa44f
Abraham Tiruneh <abraham.tiruneh7@gmail.com>
2026-03-23 22:14:03 +0000

    commit updates of the project's templates and views

--- bookstore_analytics_dashboard/Bookstore_app/urls.py
+++ bookstore_analytics_dashboard/Bookstore_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("after2010/", views.after_2010, name="books_after_2010"),
-    path("python/", views.python_books, name="python_books"),
-    path("usa_authors/", views.usa_authors, name="usa_authors"),
-    path("books_authors/", views.books_with_authors, name="books_with_authors"),
-    path("expensive_authors/", views.expensive_authors, name="expensive_authors"),
-    path("dashboard/", views.dashboard, name="dashboard"),
+    path('books/after-2010/', views.after_2010, name='after_2010'),
+    path('books/python/', views.python_books, name='python_books'),
+    path('authors/usa/', views.usa_authors, name='usa_authors'),
+    path('books/with-authors/', views.books_with_authors, name='books_with_authors'),
+    path('authors/expensive/', views.expensive_authors, name='expensive_authors'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
